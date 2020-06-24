cb78dbacf878d6ebfff9599a0fb35428d2cf3394
Vihara Girihagama <viharag21@icloud.com>
2020-06-24 14:17:03 +0530

    Make the button but no functionality

--- BrockTherapy/ViewController.swift
+++ BrockTherapy/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var holdButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        holdButton.layer.cornerRadius = 150
+        holdButton.layer.borderWidth = 1.0
+        holdButton.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        holdButton.clipsToBounds = true
     }
 
 
