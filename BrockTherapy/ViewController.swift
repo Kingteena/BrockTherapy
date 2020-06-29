68182ad0034a2e9ce5e8f26240fa55879850af8b
Vihara Girihagama <viharag21@icloud.com>
2020-06-29 22:55:37 +0530

    Add long Press and Vibrate Fuctionality

--- BrockTherapy/ViewController.swift
+++ BrockTherapy/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
     
@@ -19,8 +20,21 @@ class ViewController: UIViewController {
         holdButton.layer.borderWidth = 1.0
         holdButton.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         holdButton.clipsToBounds = true
-    }
+        
+        
+       let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+       recognizer.minimumPressDuration = 2.0 // Duration
+       holdButton.addGestureRecognizer(recognizer)
 
+            
+    }
+        
+    @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        }
+
+    }
 
 }
 
